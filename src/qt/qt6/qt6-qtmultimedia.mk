d72ea9a2a6fc7ec0342edea209d65e41999701e6
Moritz Bunkus <mo@bunkus.online>
2022-11-05 18:49:36 +0100

    Qt6: qtmultimedia: don't build ffmpeg

--- src/qt/qt6/qt6-qtmultimedia.mk
+++ src/qt/qt6/qt6-qtmultimedia.mk
@@ -6,7 +6,7 @@ PKG := qt6-qtmultimedia
 $(eval $(QT6_METADATA))
 
 $(PKG)_CHECKSUM := 34f561fdc07b158bcc4ad040b596fc6086c48908060e854b473b557e4feb1569
-$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtshadertools ffmpeg
+$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtshadertools
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
 QT6_QT_CMAKE = '$(QT6_PREFIX)/$(if $(findstring mingw,$(TARGET)),bin,libexec)/qt-cmake-private' \
